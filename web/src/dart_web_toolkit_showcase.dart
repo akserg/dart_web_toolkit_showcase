1a4396dbf00c701fcaa59f484c8d35fc60fb8242
Sergey Akopkokhyants <akserg@gmail.com>
2013-03-04 21:13:56 +0200

    Showcase 2

--- web/src/dart_web_toolkit_showcase.dart
+++ web/src/dart_web_toolkit_showcase.dart
@@ -31,29 +31,49 @@ import 'package:dart_web_toolkit/i18n.dart' as i18n;
 part 'widget_model.dart';
 
 void main() {
-  // Create split layout panel
-  ui.SplitLayoutPanel splitPanel = new ui.SplitLayoutPanel();
-  // Add split panel to root layout panel
-  ui.RootLayoutPanel.get().add(splitPanel);
-
-  // Create a static tree
-  ui.Tree staticTree = new ui.Tree();
-  staticTree.setAnimationEnabled(true);
-  // Create a container to hold the tree
-  ui.ScrollPanel staticTreeWrapper = new ui.ScrollPanel(staticTree);
-  staticTreeWrapper.setSize("100%", "100%");
+  // Main panel
+  ui.DockLayoutPanel mainPanel = new ui.DockLayoutPanel(util.Unit.PX);
+  // Add main panel to root layout panel
+  ui.RootLayoutPanel.get().add(mainPanel);
   
-  // Add staticTree wrapper to west side of split panel
-  splitPanel.addWest(staticTreeWrapper, 200.0);
-
-  // Create show placeholder
-  ui.VerticalPanel panel = new ui.VerticalPanel();
-  panel.setSize("100%", "100%");
-  panel.setVerticalAlignment(i18n.HasVerticalAlignment.ALIGN_MIDDLE);
-  panel.setHorizontalAlignment(i18n.HasHorizontalAlignment.ALIGN_CENTER);
-  splitPanel.add(panel);
+  // Create Top content
+  ui.HorizontalPanel topPanel = new ui.HorizontalPanel();
+  topPanel.addStyleName("topPanel");
+  topPanel.setSize("100%", "100%");
+  // Add top panel to main
+  mainPanel.addNorth(topPanel, 30.0);
+  // Add Logo
+  topPanel.add(new ui.Image("img/logo.png"));
+  // Add Product name
+  topPanel.add(new ui.Label("DWT showcase"));
+  // Add Product version
+  topPanel.add(new ui.Label("0.2"));
   
-  panel.add(new ui.Html("Select Widget to show"));
+  
+  
+//  // Create split layout panel
+//  ui.SplitLayoutPanel splitPanel = new ui.SplitLayoutPanel();
+//  // Add split panel to root layout panel
+//  ui.RootLayoutPanel.get().add(splitPanel);
+//
+//  // Create a static tree
+//  ui.Tree staticTree = new ui.Tree();
+//  staticTree.setAnimationEnabled(true);
+//  // Create a container to hold the tree
+//  ui.ScrollPanel staticTreeWrapper = new ui.ScrollPanel(staticTree);
+//  staticTreeWrapper.setSize("100%", "100%");
+//  
+//  // Add staticTree wrapper to west side of split panel
+//  splitPanel.addWest(staticTreeWrapper, 200.0);
+//
+//  // Create show placeholder
+//  ui.VerticalPanel panel = new ui.VerticalPanel();
+//  panel.setSize("100%", "100%");
+//  panel.setVerticalAlignment(i18n.HasVerticalAlignment.ALIGN_MIDDLE);
+//  panel.setHorizontalAlignment(i18n.HasHorizontalAlignment.ALIGN_CENTER);
+//  splitPanel.add(panel);
+//  
+//  panel.add(new ui.Html("Select Widget to show from tree"));
 
 //  // Creat command dispatcher
 //  CommandDispatcher dispatcher = new CommandDispatcher(frame);
